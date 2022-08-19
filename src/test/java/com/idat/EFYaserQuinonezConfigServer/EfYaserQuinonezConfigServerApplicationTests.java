576d599f484d4ceb0e2ed39aa8e3e766735ba3d3
yaser <yandre13.elsf@gmail.com>
2022-08-18 23:10:41 -0500

    add config server

---
+++ src/test/java/com/idat/EFYaserQuinonezConfigServer/EfYaserQuinonezConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFYaserQuinonezConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfYaserQuinonezConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
